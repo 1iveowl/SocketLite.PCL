687f660192104ed2b98c5ba96bdf2269163c5d5e
1iveowl <liveowl@outlook.com>
2016-05-26 10:06:58 +0200

    Nuget update to 0.9.5

--- src/test/SocketListe.Console.NET.Tests/Program.cs
+++ src/test/SocketListe.Console.NET.Tests/Program.cs
@@ -17,10 +17,8 @@ namespace SocketListe.Console.NET.Tests
             StartTcpListener();
             StartUdpMulticastClient();
 
-            var communicationInterface = new CommunicationInterface();
-            var allInterfaces = communicationInterface.GetAllInterfaces();
+           
 
-            var firstUsableInterface = allInterfaces.FirstOrDefault(x => x.IsUsable);
 
 
             System.Console.ReadKey();
@@ -31,14 +29,24 @@ namespace SocketListe.Console.NET.Tests
             var tcpClient = new TcpSocketClient();
             await tcpClient.ConnectAsync("192.168.1.100", 1234);
 
+
+var communicationInterface = new CommunicationInterface();
+var allInterfaces = communicationInterface.GetAllInterfaces();
+
+var firstUsableInterface = allInterfaces.FirstOrDefault(x => x.IsUsable);
+
+var udpReceived = new UdpSocketReceiver();
+await udpReceived.StartListeningAsync(
+    port:1234, 
+    communicationInterface:firstUsableInterface, 
+    allowMultipleBindToSamePort: true);
+
             var helloWorld = "Hello World!";
 
             var bytes = Encoding.UTF8.GetBytes(helloWorld);
             await tcpClient.WriteStream.WriteAsync(bytes, 0, bytes.Length);
             tcpClient.Disconnect();
             tcpClient.Dispose();
-
-            var communicationInterface = new CommunicationInterface();
         }
 
         private static async void UdpListener()
